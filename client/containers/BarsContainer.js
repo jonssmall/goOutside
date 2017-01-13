4de4665dc2b27e1591fa4d4f2c02c5b71c0b7584
Jon S <jonssmall@gmail.com>
2017-01-12 22:15:44 -0500

    search form

--- client/containers/BarsContainer.js
+++ client/containers/BarsContainer.js
@@ -1,32 +1,52 @@
 const React = require('react');
-const Bar = require('../components/BarCard.js');
+const Bars = require('../components/Bars.js');
 const api = require('../helpers/barHelpers.js');
 
-var BarsContainer = React.createClass({
+let BarsContainer = React.createClass({
     getInitialState: function() {
         return {
-            bars: []            
+            city: '',
+            bars: []      
         }
     },    
     getBars: function(location) {                 
         api.getBars(location)
         .then(result => {            
             if(result.data) {
-                console.log(result.data);
-                // this.setState({
-                //     bars: result.data
-                // });
+                //console.log(result.data);
+                this.setState({
+                    bars: result.data.businesses
+                });                
             }
         });
+    },
+    updateInput: function(e) {        
+        this.setState({
+            city: e.target.value
+        })
     },
     render: function () {
         return (
             <div>
                 <p>Hello Bars {this.state.bars.length}</p>
-                <button onClick={this.getBars.bind(null, 'norfolk')}>Click here.</button>
+                <SearchForm 
+                    handleClick={this.getBars.bind(null, this.state.city)}
+                    handleUpdate={this.updateInput}
+                    city={this.state.city} />                        
             </div>
         )
     }
 });
+
+function SearchForm(props) {
+    return (
+        <div>
+            <input onChange={props.handleUpdate}
+                value={props.city}        
+                type="text" />
+            <button onClick={props.handleClick}>Click here.</button>
+        </div>
+    )    
+}
 
 module.exports = BarsContainer;

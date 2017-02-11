a2dbca944518492e4d22b25ccca9ae29b4ae5f1b
Jon Small <jonssmall@gmail.com>
2017-02-11 00:50:16 -0500

    environmental tweak

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var app = express();
 require('dotenv').load();
 require('./server/config/passport')(passport);
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGOLAB_URI || process.env.MONGO_URI);
 mongoose.Promise = global.Promise;
 
 app.use(bodyParser.json());
